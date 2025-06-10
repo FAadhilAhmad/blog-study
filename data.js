9e71e527c6e916015b046fb49992ddf5e6a7429f
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-06-10 22:08:57 +0700

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,6 @@ let introVisible = true;
 function btnIntro() {
     const intro1 = document.getElementById("intro");
     const intro2 = document.getElementById("intro2");
-    const btnIntro = document.getElementById("btnIntro");
     if (introVisible) {
         intro1.classList.add('container-none');
         intro2.classList.remove('container-none');
@@ -14,5 +13,6 @@ function btnIntro() {
     introVisible = !introVisible;
 }
 window.onload =function () {
-    document.getElementById("intro").classList.remove("container-none");
+    document.getElementById('intro').classList.add("container-none");
+    document.getElementById("intro2").classList.remove("container-none");
 }
