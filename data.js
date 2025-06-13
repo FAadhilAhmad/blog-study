c3e3e664031e8f062e95db8a5994027757191371
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-06-13 21:45:53 +0700

    Update data.js

--- data.js
+++ data.js
@@ -16,3 +16,23 @@ window.onload =function () {
     document.getElementById('intro').classList.add("container-none"); //untuk menyembunykan intro1
     document.getElementById("intro2").classList.remove("container-none");
 }
+ let viewTeck = true;
+
+ function btnViewTeck() {
+    const layer1 = document.getElementById('iconTeck');
+    const layer2 = document.getElementById('layer2');
+
+   if (viewTeck){
+        layer1.classList.remove('non-view');
+        layer2.classList.add('non-view');
+   }else{
+        layer1.classList.add('non-view');
+        layer2.classList.remove('non-view');
+   }
+   viewTeck = !viewTeck;
+ }
+ window.onload = function () {
+    document.getElementById('iconTeck').classList.remove('non-view'); //untuk menyembunyikan iconTeck
+    document.getElementById('layer2').classList.add('non-view');
+ }
+
