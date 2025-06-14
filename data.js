d8e1707805a15692628a901ec07049b2e7153c1b
fadfadhil <fadhilsyawaludin5@gmail.com>
2025-06-14 22:27:29 +0700

    Update data.js
    remove ulang bagian js

--- data.js
+++ data.js
@@ -1,38 +1,32 @@
-let introVisible = true;
-
-function btnIntro() {
-    const intro1 = document.getElementById("intro");
-    const intro2 = document.getElementById("intro2");
-    if (introVisible) {
-        intro1.classList.add('container-none');
-        intro2.classList.remove('container-none');
-    }else {
-        intro1.classList.remove('container-none');
-        intro2.classList.add('container-none');
-    }
-    introVisible = !introVisible;
-} // fuction untuk tombol play intro 
-window.onload =function () {
-    document.getElementById('intro').classList.add("container-none"); //untuk menyembunykan intro1
-    document.getElementById("intro2").classList.remove("container-none");
-}
- let viewTeck = true;
-
- function btnViewTeck() {
+document.addEventListener('DOMContentLoaded', function() {
+    // instalasi untuk intro section
+    let introVisible = false;
+    const intro1 = document.getElementById('intro');
+    const intro2 = document.getElementById('intro2');
+    const tombolIntro = document.querySelector('.btn-intro');
+    // instalasi untuk section view
+    let viewVisible = false;
     const layer1 = document.getElementById('iconTeck');
-    const layer2 = document.getElementById('layer2');
+    const layer2 = document.getElementById ('layer2');
 
-   if (viewTeck){
-        layer1.classList.remove('non-view');
-        layer2.classList.add('non-view');
-   }else{
-        layer1.classList.add('non-view');
-        layer2.classList.remove('non-view');
-   }
-   viewTeck = !viewTeck;
- }
- window.onload = function () {
-    document.getElementById('iconTeck').classList.remove('non-view'); //untuk menyembunyikan iconTeck
-    document.getElementById('layer2').classList.add('non-view');
- }
+    // set state awal
+    intro1.classList.add('container-none');
+    intro2.classList.remove('container-none');
 
+    layer1.classList.add('non-view');
+    layer2.classList.remove('non-view');
+
+  // Fungsi untuk toggle intro
+    tombolIntro.addEventListener('click', function() {
+        intro1.classList.toggle('container-none');
+        intro2.classList.toggle('container-none');
+        
+        // Update teks tombol
+        if(intro1.classList.contains('container-none')) {
+            tombolIntro.textContent = 'Play Introduction';
+        } else {
+            tombolIntro.textContent = 'Hide Introduction';
+        }
+    });
+   
+})
